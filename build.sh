30a43dc3f6e9bcd06a7b618d8e4d3028d2583e34
Ant_Rob <rapiza06@gmail.com>
2021-06-14 17:27:46 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-HOME=$HOME
+#HOME=$HOME
 
-mkdir -p $HOME/prueba/
+#mkdir -p $HOME/prueba/
 
-WORK_DIR = $HOME/prueba/
+#WORK_DIR = $HOME/prueba/
 
-cd $WORK_DIR
+#cd $WORK_DIR
+cd $HOME
 
-git clone https://github.com/rapiza/raices-polinomio-qt.git $WORK_DIR/py-test
+git clone https://github.com/rapiza/raices-polinomio-qt.git py-test
 
-cd $WORK_DIR/py-test
+cd ~py-test
 
 pyenv virtualenv 3.7.0 venv
 pip install --upgrade pip
 pip install -r requeriments.txt
 
-python3 ./Dns_GUI3.py
+python3 ~/py-test/Dns_GUI3.py

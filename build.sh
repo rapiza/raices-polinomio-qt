18a05c67e97da9032f4ac03ca2db9b0f7450da4a
Ant_Rob <rapiza06@gmail.com>
2021-06-14 17:25:11 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ WORK_DIR = $HOME/prueba/
 
 cd $WORK_DIR
 
-git clone https://github.com/rapiza/raices-polinomio-qt.git py-test
+git clone https://github.com/rapiza/raices-polinomio-qt.git $WORK_DIR/py-test
 
-cd ./py-test
+cd $WORK_DIR/py-test
 
 pyenv virtualenv 3.7.0 venv
 pip install --upgrade pip
